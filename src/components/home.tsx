60599c360733cb097c82f7c720bc498b81539e4d
Ajeet Shah <ajeets.shah@hotmail.com>
2020-06-09 02:55:09 +0530

    history push

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 function Home() {
+  const history = useHistory()
+  console.debug(history)
   return (
     <>
       <div>Home</div>
+      <button onClick={() => history.push('/')}>Home</button>
+      <button onClick={() => history.push('/about')}>About</button>
+      <button onClick={() => history.push('/account')}>Account</button>
+      <button onClick={() => history.push('/account?name=netflix')}>
+        Netflix
+      </button>
+      <button onClick={() => history.push('/account?name=zillow-group')}>
+        Zillow Group
+      </button>
     </>
   )
 }
