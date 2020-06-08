47411fdfa708151683f499114581675280ae8cdd
Ajeet Shah <ajeets.shah@hotmail.com>
2020-06-09 02:46:21 +0530

    Add log

--- src/components/account.tsx
+++ src/components/account.tsx
@@ -4,7 +4,7 @@ import { useLocation } from 'react-router-dom'
 function Account() {
   const location = useLocation()
   const searchParams = new URLSearchParams(location.search)
-  console.debug(searchParams, location)
+  console.debug(searchParams, location, window.location.search)
   return (
     <>
       <div>Account</div>
