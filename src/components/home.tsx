6ef94b0ae394acf17590fc6c2a707e839c304eaa
Ajeet Shah <ajeets.shah@hotmail.com>
2020-06-09 02:56:04 +0530

    history push

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 function Home() {
+  const history = useHistory()
+  console.debug(history)
   return (
     <>
       <div>Home</div>
+      <button onClick={() => history.push('/')}>Home</button>
+      <button onClick={() => history.push('/about')}>About</button>
+      <button onClick={() => history.push('/person')}>Person</button>
+      <button onClick={() => history.push('/person?id=1')}>Person 1</button>
+      <button onClick={() => history.push('/person?id=2')}>Person 2</button>
     </>
   )
 }
